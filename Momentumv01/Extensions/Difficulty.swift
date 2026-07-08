fbaa9e837f67383d06966999365bd6fb4121a7f3
atommnava <atommunoznava@gmail.com>
2026-07-08 15:57:56 -0600

    Implementando Difficulty lógica

---
+++ Momentumv01/Extensions/Difficulty.swift
@@ -0,0 +1,8 @@
+//
+//  Difficulty.swift
+//  Momentumv01
+//
+//  Created by Atom Alexander Munoz Nava on 08/07/26.
+//
+
+import Foundation
---
+++ Momentumv01/Game/GameConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  GameConfiguration.swift
+//  Momentumv01
+//
+//  Created by Atom Alexander Munoz Nava on 08/07/26.
+//
+
+import Foundation
